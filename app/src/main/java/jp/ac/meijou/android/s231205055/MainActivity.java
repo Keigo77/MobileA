4b922fcc9b910c7c4989f22b8af96d68ec0c90ef
Keigo77 <147687878+Keigo77@users.noreply.github.com>
2024-09-03 12:16:53 +0900

    画像を2枚表示

--- app/src/main/java/jp/ac/meijou/android/s231205055/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205055/MainActivity.java
@@ -4,17 +4,34 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+
+import jp.ac.meijou.android.s231205055.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.text.setText(R.string.text);
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        // カメラの画像をファイルから取得
+        var cameraImage = ContextCompat.getDrawable(this, R.drawable.baseline_add_a_photo_24);
+        // imageView2に画像を代入
+        binding.imageView2.setImageDrawable(cameraImage);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
